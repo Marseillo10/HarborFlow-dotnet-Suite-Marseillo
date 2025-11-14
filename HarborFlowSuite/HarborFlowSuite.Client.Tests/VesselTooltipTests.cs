046e88c983d42549e98322239154e2ebfe4b4e1f
Marseillo Satrian <marsellorafaelbintangsatrian@gmail.com>
2025-11-15 00:26:37 +0700

    Initial commit

--- HarborFlowSuite/HarborFlowSuite.Client.Tests/VesselTooltipTests.cs
+++ HarborFlowSuite/HarborFlowSuite.Client.Tests/VesselTooltipTests.cs
@@ -37,7 +37,7 @@ namespace HarborFlowSuite.Client.Tests
             // Arrange
             var vessel = new VesselPositionDto
             {
-                VesselId = Guid.NewGuid(), // Use Guid for VesselId
+                VesselId = "123456789", // Use string for VesselId
                 VesselName = "Test Vessel",
                 VesselType = "Cargo", // Added VesselType
                 IMO = "IMO1234567",
@@ -71,7 +71,7 @@ namespace HarborFlowSuite.Client.Tests
             // Arrange
             var vessel = new VesselPositionDto
             {
-                VesselId = Guid.NewGuid(), // Use Guid for VesselId
+                VesselId = "123456789", // Use string for VesselId
                 VesselName = "Test Vessel",
                 VesselType = "Cargo", // Added VesselType
                 IMO = "IMO1234567",
@@ -98,12 +98,12 @@ namespace HarborFlowSuite.Client.Tests
         }
 
         [Fact]
-        public void VesselMap_ShowVesselTooltip_UpdatesStateAndRendersTooltip()
+        public async Task VesselMap_ShowVesselTooltip_UpdatesStateAndRendersTooltip()
         {
             // Arrange
             var vessel = new VesselPositionDto
             {
-                VesselId = Guid.NewGuid(),
+                VesselId = "123456789",
                 VesselName = "Test Vessel",
                 VesselType = "Cargo",
                 IMO = "IMO1234567",
@@ -120,7 +120,7 @@ namespace HarborFlowSuite.Client.Tests
             var cut = Render<VesselMap>();
 
             // Act
-            cut.Instance.ShowVesselTooltip(vessel, x, y);
+            await cut.InvokeAsync(() => cut.Instance.ShowVesselTooltip(vessel, x, y));
             cut.Render(); // Re-render the component to reflect state changes
 
             // Assert
@@ -132,12 +132,12 @@ namespace HarborFlowSuite.Client.Tests
         }
 
         [Fact]
-        public void VesselMap_HideVesselTooltip_UpdatesStateAndHidesTooltip()
+        public async Task VesselMap_HideVesselTooltip_UpdatesStateAndHidesTooltip()
         {
             // Arrange
             var vessel = new VesselPositionDto
             {
-                VesselId = Guid.NewGuid(),
+                VesselId = "123456789",
                 VesselName = "Test Vessel",
                 VesselType = "Cargo",
                 IMO = "IMO1234567",
@@ -152,11 +152,11 @@ namespace HarborFlowSuite.Client.Tests
             var y = 200;
 
             var cut = Render<VesselMap>();
-            cut.Instance.ShowVesselTooltip(vessel, x, y); // Show tooltip first
+            await cut.InvokeAsync(() => cut.Instance.ShowVesselTooltip(vessel, x, y)); // Show tooltip first
             cut.Render();
 
             // Act
-            cut.Instance.HideVesselTooltip();
+            await cut.InvokeAsync(() => cut.Instance.HideVesselTooltip());
             cut.Render(); // Re-render the component to reflect state changes
 
             // Assert
